fdaa38a90d535689f93e15394f435bf093a9f883
Lia Forgione <liaforg@gmail.com>
2017-07-10 15:48:43 -0400

    step 3

--- ajax.js
+++ ajax.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var runAjaxRequest = document.getElementById('run_ajax_request');
+  var runPingPong = document.getElementById('run_ping_pong');
 
   /* Your code goes here */
 
@@ -11,9 +12,27 @@ document.addEventListener("DOMContentLoaded", function() {
       method: 'GET',
       dataType: 'text',
     });
-
   });
 
+  runPingPong.addEventListener('click', function(){
 
+    $.ajax({
+      url: ' http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text',
+      }).done(function (responseData){
+      console.log(responseData);
+
+      var element = document.createElement('div');
+      element.innerHTML = responseData;
+      document.querySelector('#step3456').append(element);
+      }).fail(function( jqXHR, textStatus, errorThrown){
+      console.log('inside .fail()');
+      console.log(textStatus, errorThrown);
+      }).always(function(){
+      console.log('inside .always()');
+      });
+
+  });
 
 });
